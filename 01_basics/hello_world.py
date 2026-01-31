8241d0681700ccd9b170e302ae8bceacb3675da9
Aditya Hiremath <adityahiremath05@gmail.com>
2026-01-31 13:38:06 +0530

    First program inisde 01_basics hellow_world.py

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,8 @@
+def main(): # --> Function definition
+    print("Hello World! Welcome to Python programming.") # --> Print statement
+
+if __name__ == "__main__": # --> Entry point
+    main() # --> Call the defined function
+
+# This is a simple Python program that prints "Hello World! Welcome to Python programming." to the console.
+#  Execute this like python hello_world.py to see the output.
